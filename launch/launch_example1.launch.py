249e8807cfcd6a28ae5a4ab178a3cc70f3c9911b
gunter-png <contibali@gmail.com>
2024-11-07 09:17:00 +0100

    modified:   CMakeLists.txt 	modified:   README.md 	modified:   launch/launch_example1.launch.py 	modified:   package.xml

--- launch/launch_example1.launch.py
+++ launch/launch_example1.launch.py
@@ -25,7 +25,7 @@ def generate_launch_description():
             ]
         ),
         # Node(
-        #     package='ros2_cpp_template',
+        #     package='miniracecar',
         #     executable='simple_sub_node',
         #     output='screen',
         # ),
